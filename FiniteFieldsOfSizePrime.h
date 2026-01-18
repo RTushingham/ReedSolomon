12ce4e5db05e2d0e3038b4985a1b029dafe4cb8a
RTushingham <ralph.tush@btinternet.com>
2026-01-18 21:41:11 +0000

    Brushed off this old project. Can now create elements of finite fields of size p, complete with division. Now that I understand c++ better it's easier to engage with the maths as opposed to trying to work out how best to use the langages features to achieve what I want to.

---
+++ FiniteFieldsOfSizePrime.h
@@ -0,0 +1,131 @@
+#include "Elementary Number Theory/Typedef.h"
+
+#include <vector>
+
+// As part of building the ability to create any read solomon codes we need to create elements of finite fields (aka Galois Fields)
+//   All finite fields are of the size p**k where p and k are positive integers, and p is a prime.
+//   If k > 1 then the elements need to be expressable as polynomials modulo an irriducible polynomial.
+//   I'm going to start off with finite fields of size p where p is prime, because this is just integers modulo p.
+
+template <integer Prime>
+class ElementOfFiniteFieldP
+{
+	// static_assert( IsPrime(Prime) );
+	
+public:
+	integer value;
+	
+public:
+	ElementOfFiniteFieldP(integer InputValue)
+		: value {(InputValue + Prime) % Prime}
+	{};
+	
+	bool operator==(const ElementOfFiniteFieldP<Prime>& a){
+		return ( value + a.value) % Prime;
+	}
+	
+	ElementOfFiniteFieldP<Prime> operator+(const ElementOfFiniteFieldP<Prime>& a){
+		return ( value + a.value) % Prime;
+	}
+	ElementOfFiniteFieldP<Prime> operator-(const ElementOfFiniteFieldP<Prime>& a){
+		return (Prime - a.value + value) % Prime;
+	}
+	ElementOfFiniteFieldP<Prime> operator*(const ElementOfFiniteFieldP<Prime>& a){
+		return ((value * a.value) % Prime);
+	}
+	
+	ElementOfFiniteFieldP<Prime> FindMultiplicativeInverse()
+	{
+		// This is necessary for Fp to be a field
+		//   For all n such that 0 < n < p, we have that n and p are coprime.
+		//   Because they are coprime the Greatest Common Denominator is 1.
+		//   Using the Extended Euclidean Algorithm we can find an a and b such that a*p + b*n = 1.
+		//   Which is equivalent to 1 = a*p + (-b)n which implies (-b)n = 1 modulo p
+		//   So ( -b % p ) will be multiplicative inverse of n within this finite field.
+		
+		if( value==0 )
+		{
+			throw;
+		}
+		
+		std::vector<integer> remainders{ Prime, value };
+		std::vector<integer> multiplying_factors{};
+		
+		integer larger_remainder = remainders.at(0);
+		integer smaller_remainder = remainders.at(1);
+		do
+		{
+			remainders.push_back( larger_remainder % smaller_remainder );
+			multiplying_factors.push_back( larger_remainder / smaller_remainder );
+			
+			larger_remainder = smaller_remainder;
+			smaller_remainder = remainders.back();
+		}
+		while(remainders.back() != 0);
+		
+		// I'm going to track things in the form:
+		//   pMultiplyer[i]*p + nMultiplyer[i]*n = remainder[i]  (where this is valid from i=0 onwards)
+		//
+		// Because
+		//   remainders[i] = remainders[i+1] * multiplying_factors[i] + remainders[i+2]
+		// we have
+		//   p = n * multiplying_factors[0] + remainders[2]
+		//    <=>
+		//   1*p + (-multiplying_factors[0]) * n = remainders[2]
+		std::vector<integer> pMultiplyer{ 1, 0 };
+		//   ( next == 1 )
+		std::vector<integer> nMultiplyer{ 0, 1 };
+		//   ( next == -multiplying_factors[0] )
+		
+		// Because for all i >= 0
+		//   remainders[i] = remainders[i+1] * multiplying_factors[i] + remainders[i+2]
+		// and for all i >= 0
+		//   pMultiplyer[i]*p + nMultiplyer[i]*n = remainder[i]  (where this is valid from i=0 onwards)
+		// we get
+		//   
+		
+		for( std::size_t multiplying_factors_index = 0; multiplying_factors_index<multiplying_factors.size(); multiplying_factors_index++ )
+		{
+			// this will be at position multiplying_factors_index + 2
+			pMultiplyer.push_back( pMultiplyer.at(multiplying_factors_index) - pMultiplyer.at(multiplying_factors_index+1) * multiplying_factors.at(multiplying_factors_index) );
+			nMultiplyer.push_back( nMultiplyer.at(multiplying_factors_index) - nMultiplyer.at(multiplying_factors_index+1) * multiplying_factors.at(multiplying_factors_index) );
+		}
+		
+		std::size_t index_of_remainder_one{ (std::size_t)-1 };
+		for( std::size_t remainders_index = 0; remainders_index<remainders.size(); remainders_index++ )
+		{
+			if( remainders[remainders_index] == 1 )
+				index_of_remainder_one = remainders_index;
+		}
+		if( index_of_remainder_one == -1 )
+		{
+			// dev error
+			throw;
+		}
+		
+		// Because
+		//   pMultiplyer[index_of_remainder_one]*p + nMultiplyer[index_of_remainder_one]*n = remainder[index_of_remainder_one] = 1
+		//  =>
+		// 'the multiplicative inverse of n in this field' = nMultiplyer[index_of_remainder_one] % p
+		
+		return ElementOfFiniteFieldP<Prime>{ nMultiplyer[index_of_remainder_one] % Prime };
+	}
+	
+	ElementOfFiniteFieldP<Prime> operator/(const ElementOfFiniteFieldP<Prime>& a){
+		return (*this)*a.FindMultiplicativeInverse();
+	}
+};
+
+// To illustrate that these are Fields
+template <integer Prime>
+ElementOfFiniteFieldP<Prime> GetAdditionInvarientOfGFp()
+{
+	return ElementOfFiniteFieldP<Prime>( 0 );
+}
+// To illustrate that these are Fields
+template <integer Prime>
+ElementOfFiniteFieldP<Prime> GetMultiplicativeInvarientOfGFp()
+{
+	return ElementOfFiniteFieldP<Prime>( 1 );
+}
+
---
+++ TestSuite - FiniteFieldsOfSizePrime.cpp
@@ -0,0 +1,17 @@
+#include "FiniteFieldsOfSizePrime.h"
+
+#include <iostream>
+
+constexpr integer Prime{ 101 };
+
+int main()
+{
+	for( integer non_zero_element = 1; non_zero_element < Prime; non_zero_element++ )
+	{
+		ElementOfFiniteFieldP<Prime> a{ non_zero_element };
+		std::cout << "New value: " << a.value << std::endl;
+		std::cout << "Inverse value: " << a.FindMultiplicativeInverse().value << std::endl;
+		std::cout << "Verification value: " << (a*a.FindMultiplicativeInverse()).value << std::endl;
+	}
+}
+
